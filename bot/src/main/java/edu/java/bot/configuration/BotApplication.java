d5e202ec7e9472853b072a0faec7a57aa19c8811
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-02-18 20:05:02 +0300

    bot_token_commit_2

--- bot/src/main/java/edu/java/bot/configuration/BotApplication.java
+++ bot/src/main/java/edu/java/bot/configuration/BotApplication.java
@@ -1,5 +1,7 @@
 package edu.java.bot.configuration;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,6 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
+    @Autowired
+    ApplicationConfig applicationConfig;
+
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
     }
